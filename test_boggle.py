33962e50c29b6e11e41abf95b8ebcedca0964356
LittleBlue418 <hollyathomas88@gmail.com>
2019-11-25 15:37:13 +0100

    Added code to create a grid, and test for grid co-ordinates

--- test_boggle.py
+++ test_boggle.py
@@ -12,6 +12,24 @@ class TestBoggle(unittest.TestCase):
         grid = boggle.make_grid(0,0)
         self.assertEqual(len(grid),0)
 
+    def test_grid_size_is_width_times_height(self):
+        # Test is to ensure that the total size of the grid
+        # is equal to the width * height
+        grid = boggle.make_grid(2, 3)
+        self.assertEqual(len(grid), 6)
+
+    def test_grid_coordinates(self):
+        # Test to ensure that all of the coordinates
+        # inside of the grid can be accessed
+        grid = boggle.make_grid(2, 2)
+        self.assertIn((0, 0), grid)
+        self.assertIn((0, 1), grid)
+        self.assertIn((1, 0), grid)
+        self.assertIn((1, 1), grid)
+        self.assertNotIn((2, 2), grid)
+
+
+
 
 # class test_boggle(unittest.TestCase):
 #    def test_is_this_thing_on(self):

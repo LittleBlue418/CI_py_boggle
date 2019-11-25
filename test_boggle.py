1c6e52668d0e649ec8025755617db43fea2a45a2
LittleBlue418 <hollyathomas88@gmail.com>
2019-11-25 15:06:41 +0100

    Importing and testing unittest

---
+++ test_boggle.py
@@ -0,0 +1,6 @@
+import unittest
+
+# This class inherrits from the unittest class testcase
+class test_boggle(unittest.TestCase):
+    def test_is_this_thing_on(self):
+        self.assertEqual(1, 1)
